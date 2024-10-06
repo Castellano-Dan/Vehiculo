45cfbea865417bcc80397ac12ebb1005da3a229e
Castellano-Dan <yaizacastellano@icloud.com>
2024-10-06 16:12:54 -0600

    DEclaración de variables

--- Vehiculos/Program.cs
+++ Vehiculos/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double price;
+        string modelo;
+        string temporada;
+        int dias;
+        int i;
+    }
+}
