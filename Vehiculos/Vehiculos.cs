ea913e1ae222b2c74ce25a19e6e8ecf631ba7e7f
Castellano-Dan <yaizacastellano@icloud.com>
2024-10-06 16:04:17 -0600

    Se inicializan las variable y se agragan propiedades. Asi mismo se crea un metodo para calcular el descuento segun temporada.

--- Vehiculos/Vehiculos.cs
+++ Vehiculos/Vehiculos.cs
@@ -14,6 +14,40 @@ namespace Vehiculos
         public int temporada { get; set; }
         public int Dias { get; set; }
 
-    }
+        public Vehiculo(string Modelo1, double Precio1, int Temporada)
+        {
+            Modelo = Modelo1;
+            Precio = Precio1;
+            temporada = Temporada;
+            Descuento descuento = new Descuento(Precio, temporada);
 
+            result = descuento.PrecioDescuento;
+
+
+
+
+        }
+
+        internal class Descuento
+        {
+            public double PrecioDescuento { get; set; }
+            public Descuento(double Precio2, int j)
+            {
+                switch (j)
+                {
+                    case 1:
+                        PrecioDescuento = Precio2 * (1 - 0.15);
+                        break;
+                    case 2:
+                        PrecioDescuento = Precio2 * (1 - 0.25);
+                        break;
+                    case 3:
+                        PrecioDescuento = Precio2 * (1 - 0.50);
+                        break;
+                }
+            }
+
+        }
+    }
 }
+
