52c7b95a3301b2ce6b3bca0f3f388620033646cd
Castellano-Dan <yaizacastellano@icloud.com>
2024-10-06 15:53:24 -0600

    Creación de la clase vehiculo y sus propiedades, incluyendo las prpiedades para el descuento

---
+++ Vehiculos/Vehiculos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos
+{
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public double Precio { get; set; }
+        public double result { get; set; }
+        public int temporada { get; set; }
+        public int Dias { get; set; }
+
+    }
+
+}
