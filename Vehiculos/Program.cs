5e82eeb91cd0b1b1620efe474f13c67983aa7150
Castellano-Dan <yaizacastellano@icloud.com>
2024-10-06 16:13:46 -0600

    Solicitamos la info al usuario.

--- Vehiculos/Program.cs
+++ Vehiculos/Program.cs
@@ -10,5 +10,19 @@ public class Program
         string temporada;
         int dias;
         int i;
+
+
+
+        Console.WriteLine("Digite el precio del vehiculo");
+        price = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite el modelo:");
+        modelo = Console.ReadLine();
+
+        Console.WriteLine("Digite cuántos días reservará el vehiculo:");
+        dias = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-Digite la temporada: \n 1.-Invierno \n 2.-Verano \n 3.-Otoño");
+        i = Convert.ToInt32(Console.ReadLine());
     }
 }
