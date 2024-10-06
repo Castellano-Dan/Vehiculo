b8c63c9d7775ca248ad2388859d5e2425726b6cd
Castellano-Dan <yaizacastellano@icloud.com>
2024-10-06 16:10:34 -0600

    Creación de la clase reserva para calcular la reserva por dias.

---
+++ Vehiculos/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos
+{
+
+    public static class Reserva
+    {
+
+        public static double total { get; set; }
+
+
+        public static double totalReserva(Vehiculo vehiculo, int dia)
+        {
+
+            total = Convert.ToDouble(vehiculo.result * dia);
+            return total;
+        }
+
+
+    }
+}
